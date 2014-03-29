3bcc1b1a0e1a023a7d540ed428f5c7fc0a7336b3
Mikola Lysenko <mikolalysenko@gmail.com>
2014-03-28 19:58:45 -0500

    adding files

--- rmq.js
+++ rmq.js
@@ -6,19 +6,28 @@ var makeCartesianTree = require("cartesian-tree")
 var preprocessLCA = require("least-common-ancestor")
 
 function preprocessRangeMinimumQuery(array, compare) {
+
+  if(array.length === 0) {
+    return function() { return -1 }
+  }
+
   var cartesianTree = makeCartesianTree(array, compare)
-  var lca = preprocessLCA(cartesianTree.root)
+  var lca = preprocessLCA(cartesianTree.root, function(node, child) {
+    return child !== "value"
+  })
 
   function rangeMinimumQuery(i, j) {
     if(i < 0) {
       i = 0
     }
     if((j <= i) || (i>=array.length)) {
+      console.log("out of bounds")
       return -1
     }
     var a = cartesianTree.nodes[i]
     var b = cartesianTree.nodes[Math.min(array.length-1, j-1)]
-    return lca(a,b).index
+    var ancestor = lca(a,b)
+    return ancestor.index
   }
 
   return rangeMinimumQuery
--- test/test.js
+++ test/test.js
@@ -5,14 +5,55 @@ var preprocessRMQ = require("../rmq.js")
 
 tape("range-minimum-query", function(t) {
 
-  function verify(array) {
+  function verifyRMQ(array, rmq, compare) {
+    function bruteForceRMQ(i,j) {
+      i = Math.max(i, 0)
+      j = Math.min(j, array.length)
+      var idx = -1
+      for(var k=i; k<j; ++k) {
+        if(idx < 0) {
+          idx = k
+          continue
+        }
+        if(compare(array[idx], array[k]) > 0) {
+          idx = k
+        }
+      }
+      return idx
+    }
+    for(var i=0; i<array.length; ++i) {
+      for(var j=0; j<=array.length; ++j) {
+        t.equals(rmq(i,j), bruteForceRMQ(i,j), "range: [" + i + "," + j + ")")
+      }
+    }
+    t.equals(rmq(1, 0), -1, "checking inverted ranges")
+    t.equals(rmq(array.length, 10), -1, "inverted ranges again")
+    t.equals(rmq(-10,-1), -1, "negative ranges")
+    if(array.length>0) {
+      t.equals(rmq(-Infinity, Infinity), bruteForceRMQ(0, array.length), "whole array")
+      t.equals(rmq(-1, 1), 0, "negative range")
+    }
+  }
 
-    //TODO: Run tests
+  function verify(array) {
+    verifyRMQ(array, preprocessRMQ(array), function(i,j) {
+      return i-j
+    })
+    var objects = array.map(function(num) {
+      var obj = { value: num, self: null }
+      obj.self = obj
+      return obj
+    })
+    var compare = function(a,b) {
+      return a.value-b.value
+    }
+    verifyRMQ(array, preprocessRMQ(array, compare), compare)
   }
 
   verify([9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5])
   verify([1])
   verify([])
+
 
   t.end()
 })
